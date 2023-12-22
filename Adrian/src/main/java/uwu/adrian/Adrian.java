1f5c3f43a5aa52076d03a88ff9f14c1e8062712f
Hugo-Fernandez-DAM125 <hugo.fernandezcalzado@iesmiguelherrero.com>
2023-12-22 16:01:09 +0100

    ejemplo de hola mundo

---
+++ Adrian/src/main/java/uwu/adrian/Adrian.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uwu.adrian;
+
+/**
+ *
+ * @author DAM125
+ */
+public class Adrian {
+
+    public static void main(String[] args) {
+        System.out.println("Adriaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan, cómo lo hacemos?! Ánimo!");
+    }
+}
